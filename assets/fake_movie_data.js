fbcd74cd3f0e19260222ef9f8eaa773d4edffff6
Adam <adam.e.sheaffer@gmail.com>
2020-12-29 18:31:57 -0600

    vuex chapter has movie walkthrough

---
+++ assets/fake_movie_data.js
@@ -0,0 +1,91 @@
+export const FAKE_SEARCH_RESULTS = {
+  page: 1,
+  results: [
+    {
+      id: 9714,
+      popularity: 82.397,
+      backdrop_path: "/zG8CP8iRhLo7BNBsma6GfP6gkRF.jpg",
+      poster_path: "/uDJz1HYBCUcHCJNG8eCEhWz6oFr.jpg",
+      release_date: "1997-12-12",
+      title: "Home Alone 3"
+    },
+    {
+      id: 12536,
+      popularity: 65.84,
+      backdrop_path: "/6gvocf4kU9qJyUVZEDveOtLkaMu.jpg",
+      poster_path: "/lL2Ri3PHs9V1ot3V8ZUvrtFgj9h.jpg",
+      release_date: "2002-11-03",
+      title: "Home Alone 4"
+    },
+    {
+      id: 772,
+      popularity: 186.787,
+      backdrop_path: "/1uHTuwx5h9T3XzsXijMMKybDFvZ.jpg",
+      poster_path: "/uuitWHpJwxD1wruFl2nZHIb4UGN.jpg",
+      release_date: "1992-11-19",
+      title: "Home Alone 2: Lost in New York"
+    },
+    {
+      id: 134375,
+      popularity: 51.028,
+      backdrop_path: "/3mO7fimik20gwkVNXNxbi0RY3wg.jpg",
+      poster_path: "/z9hjtl4Phl2n1l4y1IiLUVJtZsz.jpg",
+      release_date: "2012-11-25",
+      title: "Home Alone: The Holiday Heist"
+    },
+    {
+      id: 771,
+      popularity: 8.68,
+      backdrop_path: "/vaVaNrscmsG8CUKYxiwZGFNqGJo.jpg",
+      poster_path: "/9wSbe4CwObACCQvaUVhWQyLR5Vz.jpg",
+      release_date: "1990-11-16",
+      title: "Home Alone"
+    },
+    {
+      id: 252171,
+      popularity: 8.759,
+      backdrop_path: "/zhXV4pBNbHpMd5Otk4dNY2FRvvY.jpg",
+      poster_path: "/cd2rCE1nun7CESjBI8PGNEof1tb.jpg",
+      release_date: "2014-01-19",
+      title: "A Girl Walks Home Alone at Night"
+    },
+    {
+      id: 778241,
+      popularity: 2.346,
+      poster_path: "/8XFt0DBb8zisDpawGDCvM5leVI9.jpg",
+      release_date: "2020-11-27",
+      title: "The Home Alone Story"
+    }
+  ],
+  total_pages: 2,
+  total_results: 25
+};
+
+export const FAKE_MOVIE_DETAILS = {
+  id: 772,
+  budget: 18000000,
+  genres: [
+    {
+      id: 35,
+      name: "Comedy"
+    },
+    {
+      id: 10751,
+      name: "Family"
+    },
+    {
+      id: 12,
+      name: "Adventure"
+    }
+  ],
+  original_title: "Home Alone 2: Lost in New York",
+  overview:
+    "Instead of flying to Florida with his folks, Kevin ends up alone in New York, where he gets a hotel room with his dad's credit card—despite problems from a clerk and meddling bellboy. But when Kevin runs into his old nemeses, the Wet Bandits, he's determined to foil their plans to rob a toy store on Christmas eve.",
+  popularity: 186.787,
+  poster_path: "/uuitWHpJwxD1wruFl2nZHIb4UGN.jpg",
+  release_date: "1992-11-19",
+  revenue: 358994850,
+  runtime: 120,
+  tagline: "He's up past his bedtime in the city that never sleeps.",
+  title: "Home Alone 2: Lost in New York"
+};
