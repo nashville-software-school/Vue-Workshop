b5a9dfb2b536d8729ca70b9bb4c7656097542002
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-01-18 10:55:58 -0600

    add requirements for matching-game

---
+++ assets/matching_game_data.js
@@ -0,0 +1,148 @@
+export default function getLanguageCards() {
+  return [
+    {
+      id: 1,
+      name: "C#",
+      image: "csharp.png",
+      foundMatch: false
+    },
+    {
+      id: 2,
+      name: "C#",
+      image: "csharp.png",
+      foundMatch: false
+    },
+    {
+      id: 3,
+      name: "Elixer",
+      image: "elixer.png",
+      foundMatch: false
+    },
+    {
+      id: 4,
+      name: "Elixer",
+      image: "elixer.png",
+      foundMatch: false
+    },
+    {
+      id: 5,
+      name: "Golang",
+      image: "golang.png",
+      foundMatch: false
+    },
+    {
+      id: 6,
+      name: "Golang",
+      image: "golang.png",
+      foundMatch: false
+    },
+    {
+      id: 7,
+      name: "Java",
+      image: "java.png",
+      foundMatch: false
+    },
+    {
+      id: 8,
+      name: "Java",
+      image: "java.png",
+      foundMatch: false
+    },
+    {
+      id: 9,
+      name: "JavaScript",
+      image: "javascript.png",
+      foundMatch: false
+    },
+    {
+      id: 10,
+      name: "JavaScript",
+      image: "javascript.png",
+      foundMatch: false
+    },
+    {
+      id: 11,
+      name: "Kotlin",
+      image: "kotlin.png",
+      foundMatch: false
+    },
+    {
+      id: 12,
+      name: "Kotlin",
+      image: "kotlin.png",
+      foundMatch: false
+    },
+    {
+      id: 13,
+      name: "Lisp",
+      image: "lisp.png",
+      foundMatch: false
+    },
+    {
+      id: 14,
+      name: "Lisp",
+      image: "lisp.png",
+      foundMatch: false
+    },
+    {
+      id: 15,
+      name: "PHP",
+      image: "php.png",
+      foundMatch: false
+    },
+    {
+      id: 16,
+      name: "PHP",
+      image: "php.png",
+      foundMatch: false
+    },
+    {
+      id: 17,
+      name: "Python",
+      image: "python.png",
+      foundMatch: false
+    },
+    {
+      id: 18,
+      name: "Python",
+      image: "python.png",
+      foundMatch: false
+    },
+    {
+      id: 19,
+      name: "Ruby",
+      image: "ruby.png",
+      foundMatch: false
+    },
+    {
+      id: 20,
+      name: "Ruby",
+      image: "ruby.png",
+      foundMatch: false
+    },
+    {
+      id: 21,
+      name: "Rust",
+      image: "rust.png",
+      foundMatch: false
+    },
+    {
+      id: 22,
+      name: "Rust",
+      image: "rust.png",
+      foundMatch: false
+    },
+    {
+      id: 23,
+      name: "Swift",
+      image: "swift.png",
+      foundMatch: false
+    },
+    {
+      id: 24,
+      name: "Swift",
+      image: "swift.png",
+      foundMatch: false
+    }
+  ];
+}
---
+++ project-vault/matching-game.md
@@ -0,0 +1,29 @@
+# Build a Matching Game
+
+For this project, build a memory matching game which has users flip over cards to reveal images, and then try to find match that card's match.
+
+[**View a Demo**](https://nss-vue-matching-game.web.app/)
+
+> **NOTE**: I would recommend using the [lodash shuffle](https://www.npmjs.com/package/lodash.shuffle) package from npm to handle shuffling the cards. Once installed you can import it using
+>
+> ```js
+> import shuffle from "lodash.shuffle";
+> ```
+>
+> and call it like this
+>
+> ```js
+> shuffle(arrayOfCards);
+> ```
+
+## Part 1
+
+1. Download the [images](../assets/images.zip) to use for the cards. (You're also welcome to pick 12 different images if you'd rather the game use something else)
+1. Use [this data](../assets/matching_game_data.js) to generate a list of 24 cards on the screen
+1. The cards should be shuffled so they are not in any obvious order and are different every time the user plays
+1. When the application starts, all cards should appear face down. Clicking on a card should display the image (you can do this either with `v-if` to hide/show or you can use `:class` bindings to create a "flip" animation)
+1. If the user reveals 2 cards that are a match, those cards should remain face up. If a user selects a card followed by a non-matching card, the app should wait 1 second and then hide both
+
+## Part 2
+
+1. If the user matches all cards successfully, they should be shown a message at the top of the screen congratulating them.
