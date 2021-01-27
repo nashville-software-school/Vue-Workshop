6d492212d7b020b0c025ba0c7c8261de346cbfbb
Adam <adam.e.sheaffer@gmail.com>
2021-01-27 12:50:31 -0600

    spelling test req

---
+++ assets/spellingData.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    word: "Encapsulation",
+    userInput: null
+  },
+  {
+    word: "Abstraction",
+    userInput: null
+  },
+  {
+    word: "Polymorphism",
+    userInput: null
+  },
+  {
+    word: "Inheritance",
+    userInput: null
+  },
+  {
+    word: "Dependency",
+    userInput: null
+  },
+  {
+    word: "Repository",
+    userInput: null
+  },
+  {
+    word: "Graphical",
+    userInput: null
+  },
+  {
+    word: "Interface",
+    userInput: null
+  },
+  {
+    word: "Lexical",
+    userInput: null
+  },
+  {
+    word: "Iteration",
+    userInput: null
+  }
+];
